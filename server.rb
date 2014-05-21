14298d5023c41b973072a8b339b233ccb5ae676a
Eric Kelly <heroiceric@gmail.com>
2014-05-21 11:17:11 -0400

    Remove unnecessary code
    These are the default settings, so you don’t have to add them in there.
    Knowing how to set these is useful if you wanted to change them for
    some reason.

--- server.rb
+++ server.rb
@@ -45,10 +45,3 @@ get '/teams/:team_name' do
 
   erb :show
 end
-
-
-# These lines can be removed since they are using the default values. They've
-# been included to explicitly show the configuration options.
-set :views, File.dirname(__FILE__) + '/views'
-set :public_folder, File.dirname(__FILE__) + '/public'
-
